7a5a306ea9d81aec9a2f114b86d1c3a53ef68661
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2022-12-23 14:03:11 +0800

    (feat) Create Word Boundary Related Pattern Methods

---
+++ spec/patterns/contains_the_word_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require "regexer"
+require "./spec/shared_examples/shared_examples_for_contains_test"
+
+RSpec.describe "Regexer::Pattern #contains_the_word" do
+  let(:val) { nil }
+
+  let(:pattern_block) do
+    lambda do |value|
+      -> { contains_the_word value }
+    end.call(val)
+  end
+
+  subject(:pattern) do
+    Regexer::PatternBuilder.new(&pattern_block).result.regex
+  end
+
+  # NOTE: Under the hood, contains_the_word method actually uses the contains method
+  include_examples "contains method test examples", [
+    {
+      case: "when value is an exact integer: 26543",
+      test_value: 26_543,
+      expected_value: /\b(26543)\b/
+    },
+    {
+      case: "when value is an exact float: 3.56",
+      test_value: 3.56,
+      expected_value: /\b(3\.56)\b/
+    },
+    {
+      case: "when value is an exact set of characters: 'testing'",
+      test_value: "testing",
+      expected_value: /\b(testing)\b/
+    },
+    {
+      case: "when value contains regex special characters",
+      test_value: ".+*?^$()[]{}|\\",
+      custom_assertion_message: "escapes those special characters in the final generated pattern",
+      expected_value: /\b(\.\+\*\?\^\$\(\)\[\]\{\}\|\\)\b/
+    }
+  ]
+
+  context "when contains_the_word_with alias method is used" do
+    let!(:pattern_block) do
+      -> { contains_the_word_with "a" }
+    end
+
+    it "returns /\\ba\\b/ regex pattern" do
+      expect(pattern).to eq(/\ba\b/)
+    end
+  end
+
+  context "when the_word alias method is used" do
+    let!(:pattern_block) do
+      -> { the_word "a" }
+    end
+
+    it "returns /\\ba\\b/ regex pattern" do
+      expect(pattern).to eq(/\ba\b/)
+    end
+  end
+
+  context "when the_word_with alias method is used" do
+    let!(:pattern_block) do
+      -> { the_word_with "a" }
+    end
+
+    it "returns /\\ba\\b/ regex pattern" do
+      expect(pattern).to eq(/\ba\b/)
+    end
+  end
+
+  context "when single entity values are given" do
+    include_examples "contains method test examples", [
+      {
+        case: "when value given is a single character",
+        test_value: "@",
+        custom_assertion_message: "does not wrap the character in parentheses",
+        expected_value: /\b@\b/
+      }
+    ]
+  end
+
+  context "when non-single entity values are given" do
+    context "when value is a non-single entity pattern object" do
+      let(:pattern_block) do
+        -> { contains_the_word_with consecutive_instances_of letter from: "A", to: "z" }
+      end
+
+      it "wraps the pattern in parentheses" do
+        expect(pattern).to eq(/\b([A-z]+)\b/)
+      end
+    end
+  end
+
+  include_examples "contains method invalid value error test example", value: /test/
+end
---
+++ spec/patterns/contains_the_word_starting_with_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require "regexer"
+require "./spec/shared_examples/shared_examples_for_contains_test"
+
+RSpec.describe "Regexer::Pattern #contains_the_word_starting_with" do
+  let(:val) { nil }
+
+  let(:pattern_block) do
+    lambda do |value|
+      -> { contains_the_word_starting_with value }
+    end.call(val)
+  end
+
+  subject(:pattern) do
+    Regexer::PatternBuilder.new(&pattern_block).result.regex
+  end
+
+  # NOTE: Under the hood, contains_the_word_starting_with method actually uses the contains method
+  include_examples "contains method test examples", [
+    {
+      case: "when value is an exact integer: 26543",
+      test_value: 26_543,
+      expected_value: /\b(26543)/
+    },
+    {
+      case: "when value is an exact float: 3.56",
+      test_value: 3.56,
+      expected_value: /\b(3\.56)/
+    },
+    {
+      case: "when value is an exact set of characters: 'testing'",
+      test_value: "testing",
+      expected_value: /\b(testing)/
+    },
+    {
+      case: "when value contains regex special characters",
+      test_value: ".+*?^$()[]{}|\\",
+      custom_assertion_message: "escapes those special characters in the final generated pattern",
+      expected_value: /\b(\.\+\*\?\^\$\(\)\[\]\{\}\|\\)/
+    }
+  ]
+
+  context "when the_word_starting_with alias method is used" do
+    let!(:pattern_block) do
+      -> { the_word_starting_with "a" }
+    end
+
+    it "returns /\\ba/ regex pattern" do
+      expect(pattern).to eq(/\ba/)
+    end
+  end
+
+  context "when single entity values are given" do
+    include_examples "contains method test examples", [
+      {
+        case: "when value given is a single character",
+        test_value: "@",
+        custom_assertion_message: "does not wrap the character in parentheses",
+        expected_value: /\b@/
+      }
+    ]
+  end
+
+  context "when non-single entity values are given" do
+    context "when value is a non-single entity pattern object" do
+      let(:pattern_block) do
+        -> { contains_the_word_starting_with consecutive_instances_of letter from: "A", to: "z" }
+      end
+
+      it "wraps the pattern in parentheses" do
+        expect(pattern).to eq(/\b([A-z]+)/)
+      end
+    end
+  end
+
+  include_examples "contains method invalid value error test example", value: /test/
+end
