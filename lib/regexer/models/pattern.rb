2ee60c70dc2bbf6f837e9d613bd04c5db9a623f4
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2023-01-06 02:04:25 +0800

    (feat) Add Ability to Add Two Pattern Objects Together

--- lib/regexer/models/pattern.rb
+++ lib/regexer/models/pattern.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "regexer/exceptions/invalid_value_error"
+
 module Regexer
   module Models
     # The main model for the patterns being build by pattern builder
@@ -7,9 +9,19 @@ module Regexer
       attr_reader :raw_pattern
 
       def initialize(pattern, regex_escaped: true, single_entity: true)
-        @raw_pattern = pattern
+        @raw_pattern = String.new(pattern)
         @regex_escaped = regex_escaped
         @single_entity = single_entity
+      end
+
+      def +(other)
+        unless other.is_a?(Regexer::Models::Pattern)
+          raise TypeError,
+                "Unable to add #{other.class} to Regexer::Models::Pattern"
+        end
+
+        Regexer::Models::Pattern.new(@raw_pattern + other.raw_pattern, regex_escaped: @regex_escaped,
+                                                                       single_entity: @single_entity)
       end
 
       def regex_escaped?
---
+++ spec/models/pattern_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require "regexer"
+
+RSpec.describe Regexer::Models::Pattern do
+  describe "Instance" do
+    context "when given pattern value is not a string" do
+      it "raises a TypeError" do
+        expect { described_class.new(/test/) }.to raise_error(TypeError)
+      end
+    end
+
+    context "when given pattern value is a string" do
+      it "successfully creates a Pattern instance" do
+        expect(described_class.new("test")).to be_a Regexer::Models::Pattern
+      end
+
+      it "saves a new instance of the given string" do
+        given_value = "test"
+        expect(described_class.new(given_value).raw_pattern).not_to equal given_value
+      end
+    end
+
+    describe "Instance Methods" do
+      context "#+" do
+        context "when non Pattern objects are given" do
+          it "raises a TypeError" do
+            expect { described_class.new("") + 1 }.to raise_error(TypeError)
+          end
+        end
+
+        context "when Pattern objects are given" do
+          it "returns a new instance of Pattern where its value contains the added Pattern" do
+            original_pattern = described_class.new("test")
+            to_be_added_pattern = Regexer::Models::Pattern.new(" hello")
+            result = original_pattern + to_be_added_pattern
+
+            expect(result).to be_a Regexer::Models::Pattern
+            expect(result).not_to equal(original_pattern)
+            expect(result.raw_pattern).to eq("test hello")
+          end
+        end
+      end
+
+      context "#raw_pattern" do
+        it "returns its main value in string format" do
+          expect(described_class.new("test").raw_pattern).to eq "test"
+          expect(described_class.new("test").raw_pattern).to be_a String
+        end
+      end
+
+      context "#regex_escaped?" do
+        it "returns a boolean representing if a pattern is already regex escaped or not" do
+          expect(described_class.new("test").regex_escaped?).to eq true
+        end
+      end
+
+      context "#single_entity?" do
+        it "returns a boolean representing if a pattern is a single entity or not" do
+          expect(described_class.new("test").single_entity?).to eq true
+        end
+      end
+
+      context "#regex" do
+        it "returns the regex equivalent of the pattern" do
+          expect(described_class.new("test").regex).to eq(/test/)
+        end
+      end
+    end
+  end
+end
